5160f660f3564e3719146e7aa035b1d10c21badd
Daniel Reyes <daniel@econova.com.mx>
2025-11-03 19:23:02 -0600

    Actualizar paleta de colores: aplicar fondo cremoso/hueso del login y sidebar turquesa

--- client/src/components/layout/PageLayout.tsx
+++ client/src/components/layout/PageLayout.tsx
@@ -9,11 +9,11 @@ interface PageLayoutProps {
 
 export function PageLayout({ children, title }: PageLayoutProps) {
   return (
-    <div className="flex h-screen bg-gray-50">
+    <div className="flex h-screen bg-background">
       <Sidebar />
       <div className="flex flex-col flex-1 overflow-hidden ml-0 lg:ml-[250px]">
         <TopBar title={title} />
-        <main className="flex-1 overflow-y-auto bg-gray-50 px-4 md:px-6 lg:px-8">
+        <main className="flex-1 overflow-y-auto bg-background px-4 md:px-6 lg:px-8">
           {children}
         </main>
       </div>
